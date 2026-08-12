a3c59ff1721e5b33ce2f003552969cdb338a3cfe
mike parks <mike@greenshoegarage.com>
2026-08-12 00:13:32 -0400

    updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "meshboard-v0.6.3";
+const CACHE = "meshboard-v0.6.4";
 const CORE = [
   "./styles.css", "./manifest.webmanifest",
   "./app/main.js", "./app/models.js", "./app/demo.js", "./app/storage.js",
